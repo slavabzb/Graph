6c70ff9fb84b8ac88d3820cb804e094e96ff0e59
Vjacheslav Bezborodov <waww@live.ru>
2015-10-04 15:55:24 +0500

    add menu

--- Graph.cpp
+++ Graph.cpp
@@ -4,10 +4,27 @@
 #include "Graph.h"      // Файл заголовка класса Graph
 #include <fstream>      // Файловый ввод-вывод
 #include <iostream>     // Консольный ввод-вывод
+#include <sstream>      // Строковый ввод-вывод
+#include <limits>       // Диапазоны типов
 
 /*
-Функция чтения графа из файла
-Файл имеет одну из следующих структур
+Конструктор класса Graph. Инициализирует все данные-члены (поля) класса
+*/
+Graph::Graph()
+{
+    // Установить значения по умолчанию для всех полей класса
+    type = ' ';
+    weighted = false;
+    oriented = false;
+}
+
+/*
+Функция чтения графа из файла.
+Во всех представлениях вершины нумеруются с 0.
+
+Файл имеет одну из следующих структур.
+
+
 
 1. Матрица смежности
 
@@ -19,7 +36,47 @@ C 3 1
 Этот пример описывает взвешенный граф (последнее число в первой строке - 1;
 если 0, то граф невзвешенный), представленный в виде матрицы смежности
 (символ 'C' в первой строке), который имеет 3 вершины
-(второе число в первой строке)
+(второе число в первой строке).
+
+Граф имеет 2 ребра:
+* ребро с весом 2 между вершинами 0 и 1;
+* ребро с весом 5 между вершинами 0 и 2.
+
+
+
+2. Список смежности
+
+L 3
+0 0
+1 2
+0
+0
+
+Этот пример описывает неориентированный (первый 0) невзвешенный (второй 0) граф,
+представленный в виде списка смежности (символ 'L' в первой строке), который
+имеет 3 вершины (второе число в первой строке).
+
+Граф имеет 2 ребра:
+* ребро между вершинами 0 и 1;
+* ребро между вершинами 0 и 2.
+
+Другой вариант списка смежности
+
+L 3
+1 1
+1 2 2 5
+0 2
+0 5
+
+
+Этот пример описывает ориентированный (первая 1) взвешенный (вторая 1) граф,
+представленный в виде списка смежности (символ 'L' в первой строке), который
+имеет 3 вершины (второе число в первой строке).
+
+Граф имеет 2 ребра:
+* ребро с весом 2 между вершинами 0 и 1;
+* ребро с весом 5 между вершинами 0 и 2.
+
 */
 void Graph::readGraph(string fileName)
 {
@@ -33,23 +90,23 @@ void Graph::readGraph(string fileName)
         return;
     }
 
-    // Очистить текущее содержимое матрицы смежности
-    adjMatrix.clear();
+    cout << "Reading graph from file " << fileName << "...\t";
 
     // Определить, какой тип внутреннего представления графа описан в файле
-    char type = ' ';
     file >> type;
 
-    // Если в файле описана матрица смежности
+    // Если в файле описан граф, представленный в виде матрицы смежности
     if(type == 'C')
     {
+        // Очистить текущее содержимое матрицы смежности
+        adjMatrix.clear();
+
         // Считать количество вершин
         int N = 0;
         file >> N;
 
         // Считать индикатор взвешенности графа
-        bool W = false;
-        file >> W;
+        file >> weighted;
 
         // Считать всю матрицу смежности
 
@@ -74,21 +131,160 @@ void Graph::readGraph(string fileName)
             adjMatrix.push_back(row);
         }
     }
+    else    // Если в файле описан граф, представленный в виде списка смежности
+    if(type == 'L')
+    {
+        // Очистить текущее содержимое списка смежности
+        adjList.clear();
+
+        // Считать количество вершин
+        int N = 0;
+        file >> N;
+
+        // Считать индикатор ориентированности графа
+        file >> oriented;
+
+        // Считать индикатор взвешенности графа
+        file >> weighted;
+
+        // Установить курсор чтения файла на новую строку
+        file.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        // Считать N связей вершин
+        for(int from = 0; from < N; from++)
+        {
+            // Строка для хранения списка связей текущей вершины
+            string rowStr;
+
+            // Считать текущую строку
+            getline(file, rowStr);
+
+            // Строковый поток ввода (для разбиения row по отдельным числам)
+            istringstream iss(rowStr);
+
+            // Переменная для хранения связей текущей вершины
+            set< tuple< int, int, int > > row;
+
+            // Бежать по rowStr
+            do
+            {
+                // Переменная для хранения номера соседней вершины
+                int to = 0;
+
+                // Считать соседнюю вершину
+                iss >> to;
+
+                // Переменная для хранения веса ребра
+                int weight = 0;
+
+                // Если описан взвешенный граф
+                if(weighted)
+                {
+                    // Считать вес ребра
+                    iss >> weight;
+                }
+
+                // Сохранить считанные связи в список связей
+                row.insert(make_tuple(from, to, weight));
+
+            } while (!iss.eof());
+
+            // Добавить список связей считанной вершины в список смежности
+            adjList.push_back(row);
+        }
+    }
+
+    cout << "done\n";
+
+    // Напечатать граф
+    printGraph();
 }
 
 void Graph::addEdge(int from, int to, int weight)
 {
+    cout << "Adding edge from " << from << " to " << to << ", weight is " << weight << "...\t";
 
+    // Если значение параметра weight равно 0, это будет означать удаление ребра
+    if(weight == 0)
+    {
+        cout << "Can't add edge from " << from << " to " << to << ", weight is equal to zero\n";
+        return;
+    }
+
+    // Если текущее представление - матрица смежности
+    if(type == 'C')
+    {
+        if(max(from,to) > adjMatrix.size() || min(from,to) < 0)
+        {
+            cout << "\nCan't add edge from " << from << " to " << to << ", the node doesn't exists\n";
+            return;
+        }
+
+        // Установить в соответствующем столбце значение веса
+        adjMatrix[from][to] = weight;
+
+        // Установить значение симметричного элемента (свойство матрицы смежности)
+        adjMatrix[to][from] = weight;
+    }
+
+    cout << "done\n";
+
+    // Напечатать граф
+    printGraph();
 }
 
 void Graph::removeEdge(int from, int to)
 {
+    cout << "Removing edge from " << from << " to " << to << "...\t";
 
+    // Если текущее представление - матрица смежности
+    if(type == 'C')
+    {
+        if(max(from,to) > adjMatrix.size() || min(from,to) < 0)
+        {
+            cout << "\nCan't remove edge from " << from << " to " << to << ", the node doesn't exists\n";
+            return;
+        }
+
+        // Установить в соответствующем столбце значение веса в 0
+        adjMatrix[from][to] = 0;
+
+        // Установить значение симметричного элемента (свойство матрицы смежности) в 0
+        adjMatrix[to][from] = 0;
+    }
+
+    cout << "done\n";
+
+    // Напечатать граф
+    printGraph();
 }
 
 int Graph::changeEdge(int from, int to, int newWeight)
 {
-    return 0;
+    cout << "Changing edge from " << from << " to " << to << ", new weight is " << newWeight << "...\t";
+
+    int oldWeight = 0;
+
+    // Если текущее представление - матрица смежности
+    if(type == 'C')
+    {
+        // Сохранить прежнее значение веса
+        oldWeight = adjMatrix[from][to];
+
+        // Установить в соответствующем столбце значение веса в newWeight
+        adjMatrix[from][to] = newWeight;
+
+        // Установить значение симметричного элемента (свойство матрицы смежности) в newWeight
+        adjMatrix[to][from] = newWeight;
+    }
+
+    cout << "done\n";
+
+    // Напечатать граф
+    printGraph();
+
+    // Вернуть прежнее значение веса
+    return oldWeight;
 }
 
 void Graph::transformToAdjList()
@@ -106,7 +302,129 @@ void Graph::transformToListOfEdges()
 
 }
 
+void Graph::printGraph()
+{
+    cout << "Printing graph (";
+
+    // Если текущее представление - матрица смежности
+    if(type == 'C')
+    {
+        cout << "adjacency matrix)...\t\n";
+
+        // Бежать по строкам
+        for(size_t i=0; i<adjMatrix.size(); i++)
+        {
+            // Бежать по столбцам
+            for(size_t j=0; j<adjMatrix[i].size(); j++)
+            {
+                // Напечатать текущий элемент
+                cout << adjMatrix[i][j] << " ";
+            }
+            cout << "\n";
+        }
+    }
+    else    // Если текущее представление - список смежности
+    if(type == 'L')
+    {
+        cout << "adjacency list)...\t\n";
+
+        // Бежать по строкам
+        for(size_t i=0; i<adjList.size(); i++)
+        {
+            // Бежать по столбцам
+            for(set< tuple< int, int, int > >::iterator it = adjList[i].begin(); it!=adjList[i].end(); it++)
+            {
+                // Напечатать конец текущего ребра
+                cout << get<1>(*it);
+
+                // Если граф взвешенный
+                if(weighted)
+                {
+                    // Напечатать вес текущего ребра
+                    cout << " " << get<2>(*it);
+                }
+
+                cout << " ";
+            }
+
+            cout << "\n";
+        }
+    }
+
+}
+
 void Graph::writeGraph(string fileName)
 {
+    // Создать объект файла и попытаться открыть файл с именем "fileName"
+    ofstream file(fileName.c_str());
 
+    // Если не удалось открыть файл, кинуть исключение
+    if(!file.is_open())
+    {
+        cout << "Can't open file " << fileName << "\n";
+        return;
+    }
+
+    cout << "Writing graph to file " << fileName << "...\t";
+
+    // Сохранить тип текущего представления
+    file << type << " ";
+
+    // Если текущее представление - матрица смежности
+    if(type == 'C')
+    {
+        // Сохранить количество вершин
+        file << adjMatrix.size() << " ";
+
+        // Сохранить флаг взвешенности
+        file << weighted << "\n";
+
+        // Бежать по строкам
+        for(size_t i=0; i<adjMatrix.size(); i++)
+        {
+            // Бежать по столбцам
+            for(size_t j=0; j<adjMatrix[i].size(); j++)
+            {
+                // Сохранить текущий элемент
+                file << adjMatrix[i][j] << " ";
+            }
+            file << "\n";
+        }
+    }
+    else    // Если текущее представление - список смежности
+    if(type == 'L')
+    {
+        // Сохранить количество вершин
+        file << adjList.size() << "\n";
+
+        // Сохранить флаг ориентированности графа
+        file << oriented << " ";
+
+        // Сохранить флаг взвешенности графа
+        file << weighted << "\n";
+
+        // Бежать по строкам
+        for(size_t i=0; i<adjList.size(); i++)
+        {
+            // Бежать по столбцам
+            for(set< tuple< int, int, int > >::iterator it = adjList[i].begin(); it!=adjList[i].end(); it++)
+            {
+                // Сохранить конец текущего ребра
+                file << get<1>(*it);
+
+                // Если граф взвешенный
+                if(weighted)
+                {
+                    // Сохранить вес текущего ребра
+                    file << " " << get<2>(*it);
+                }
+
+                file << " ";
+            }
+
+            file << "\n";
+        }
+    }
+
+    cout << "done\n";
 }
--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 // Файл "main.cpp"
 // Основной файл программы
 
-// Подключить описание класса Graph
-#include "Graph.h"
+#include "Graph.h"  // Подключить описание класса Graph
+#include <iostream> // Ввод-вывод
+
+// Сделать все функции и объекты стандартной библиотеки "видимыми" в текущем коде
+using namespace std;
 
 // Основная функция программы
 int main()
@@ -10,8 +13,128 @@ int main()
     // Создать объект graph класса Graph
     Graph graph;
 
-    // Считать описание графа из файла file.txt
-    graph.readGraph("file.txt");
+    // Поприветствовать пользователя
+    cout << "Graph operations and algorithms.\n";
+    cout << "(C) Dremov V., SUSU, 2015.\n";
+
+    // Переменная для хранения пользовательского выбора
+    char choice = ' ';
+
+    // Опрашивать пользователя и выполнять требуемое
+    do
+    {
+        // Показать основное меню
+        cout << "\n\nWhat would you like to do?\n";
+        cout << "[1] Read graph from file\n";
+        cout << "[2] Save graph as\n";
+        cout << "[3] Add an edge\n";
+        cout << "[4] Change an edge\n";
+        cout << "[5] Remove an edge\n";
+        cout << "[0] Exit\n";
+
+        // Получить выбор пользователя
+        cin >> choice;
+
+        if(choice == '1')
+        {
+            cout << "Reading graph...\n";
+
+            // Переменная для хранения имени файла
+            string fileName;
+
+            // Получить имя файла от пользователя
+            cout << "\tEnter file name > ";
+            cin >> fileName;
+
+            // Считать граф из указанного файла
+            graph.readGraph(fileName);
+        }
+        else if(choice == '2')
+        {
+            cout << "Saving graph...\n";
+
+            // Переменная для хранения имени файла
+            string fileName;
+
+            // Получить имя файла от пользователя
+            cout << "\tEnter file name > ";
+            cin >> fileName;
+
+            // Сохранить граф в указанный файл
+            graph.writeGraph(fileName);
+        }
+        else if(choice == '3')
+        {
+            cout << "Adding an edge...\n";
+
+            // Переменные для хранения номеров вершин и веса ребра
+            int from = 0;
+            int to = 0;
+            int weight = 0;
+
+            // Получить номер первой вершины от пользователя
+            cout << "\tEnter \"from\" vertex > ";
+            cin >> from;
+
+            // Получить номер второй вершины от пользователя
+            cout << "\tEnter \"to\" vertex > ";
+            cin >> to;
+
+            // Получить значение веса нового ребра от пользователя
+            cout << "\tEnter a weight > ";
+            cin >> weight;
+
+            // Добавить ребро с указанными параметрами
+            graph.addEdge(from,to,weight);
+        }
+        else if(choice == '4')
+        {
+            cout << "Changing an edge...\n";
+
+            // Переменные для хранения номеров вершин и веса ребра
+            int from = 0;
+            int to = 0;
+            int newWeight = 0;
+            int oldWeight = 0;
+
+            // Получить номер первой вершины от пользователя
+            cout << "\tEnter \"from\" vertex > ";
+            cin >> from;
+
+            // Получить номер второй вершины от пользователя
+            cout << "\tEnter \"to\" vertex > ";
+            cin >> to;
+
+            // Получить новое значение веса редактируемого ребра от пользователя
+            cout << "\tEnter a new weight > ";
+            cin >> newWeight;
+
+            // Редактировать ребро с указанными параметрами
+            oldWeight = graph.changeEdge(from,to,newWeight);
+
+            // Напечатать прежнее значение веса
+            cout << "\tThe previous value of weight was " << oldWeight << "\n";
+        }
+        else if(choice == '5')
+        {
+            cout << "Removing an edge...\n";
+
+            // Переменные для хранения номеров вершин
+            int from = 0;
+            int to = 0;
+
+            // Получить номер первой вершины от пользователя
+            cout << "\tEnter \"from\" vertex > ";
+            cin >> from;
+
+            // Получить номер второй вершины от пользователя
+            cout << "\tEnter \"to\" vertex > ";
+            cin >> to;
+
+            // Удалить указанное ребро
+            graph.removeEdge(from, to);
+        }
+    } while(choice != '0');
 
     return 0;
 }
